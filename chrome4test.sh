3542885e778f81a872a3785a0f5144372d919343
Bucky Spires <gigabuck@gmail.com>
2015-11-03 22:56:41 -0500

    initial commit

---
+++ chrome4test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=/tmp/testing/chrome/ --proxy-server=127.0.0.1:8080 --disable-xss-auditor --disable-client-side-phishing-detection --disable-component-update --ignore-certificate-errors
